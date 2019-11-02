595e73a42a69bd7b143db39696bbde35bfee1cc3
胡立愿 <huliyuan@runxsports.com>
2019-11-02 09:04:15 +0800

    初始化

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+From centos
+
+RUN yum install gcc make zlib* wget vim openssl-devel libffi-devel -y \
+    && cd /tmp \
+    && mkdir -p /data/python3.7.5 \
+    && wget https://www.python.org/ftp/python/3.7.5/Python-3.7.5.tgz \
+    && tar zxvf Python-3.7.5.tgz \
+    && cd Python-3.7.5 \
+    && ./configure --prefix=/data/python3.7.5 --enable-optimizations --with-ssl \
+    && make \
+    && make install \
+    && yum clean all \
+    && cd /tmp \
+    && rm -rf /tmp/*
+ADD ./files/setup.sh /tmp/setup.sh
+RUN chmod 755 /tmp/setup.sh
+ENTRYPOINT ["/tmp/setup.sh"]
+CMD ["-v"]
+
---
+++ files/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ -z PYPI_DOMAIN -o -z PYPI_OPTION ];then
+    if [ ! -f /root/.pip/pip.conf ];then
+        mkdir -p /root/.pip/pip.conf
+        echo "[global]" >> /root/.pip/pip.conf
+        if [ -z PYPI_DOMAIN ];then
+            echo "index-url=${PYPI_DOMAIN}" >> /root/.pip/pip.conf
+        fi
+        if [ -z PYPI_OPTION ];then
+            echo "trusted-host=${PYPI_OPTION}" >> /root/.pip/pip.conf
+        fi
+    fi
+fi
+if [ -f requirements.txt ];then
+    /data/python3.7.5/bin/pip3 install -r requirements.txt
+fi
+exec /data/python3.7.5/bin/python3 "$@"
